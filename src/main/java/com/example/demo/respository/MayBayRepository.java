e0950f644fa1b03eb2a15b8c49c73a9cfd841d0d
caotienhoang123 <113164766+caotienhoang123@users.noreply.github.com>
2022-09-30 01:13:10 +0700

    Update MayBayRepository.java

--- src/main/java/com/example/demo/respository/MayBayRepository.java
+++ src/main/java/com/example/demo/respository/MayBayRepository.java
@@ -8,7 +8,6 @@ import org.springframework.data.jpa.repository.Query;
 import com.example.demo.entity.MayBay;
 
 public interface MayBayRepository extends JpaRepository<MayBay, Integer> {
-//	2. Cho biết các loại máy bay có tầm bay lớn hơn 10,000km.
 	@Query(value = "select loai from [dbo].[maybay] where [TamBay]>10000", nativeQuery = true)
 	public List<String> findByTamBayLonHon10000();
 
