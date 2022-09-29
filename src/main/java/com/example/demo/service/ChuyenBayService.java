7ce6ea27f9ba3acab51b2c3d1daa721b0c2ed103
caotienhoang123 <113164766+caotienhoang123@users.noreply.github.com>
2022-09-30 01:14:39 +0700

    Update ChuyenBayService.java

--- src/main/java/com/example/demo/service/ChuyenBayService.java
+++ src/main/java/com/example/demo/service/ChuyenBayService.java
@@ -5,7 +5,6 @@ import java.util.List;
 import com.example.demo.entity.ChuyenBay;
 
 public interface ChuyenBayService {
-	//1.Cho biết các chuyến bay đi Đà Lạt (DAD)
 	public List<ChuyenBay> findByGaDen(String gaDen);
 
 
